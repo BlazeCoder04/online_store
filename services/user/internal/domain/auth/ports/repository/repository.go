d62abc7aa0a53ef45d004c971138cd173e2f61a0
BlazeCoder <blaze.coder.work@gmail.com>
2025-06-19 14:29:00 +0300

    feat(user): add user model, auth ports

---
+++ services/user/internal/domain/auth/ports/repository/repository.go
@@ -0,0 +1,13 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/BlazeCoder04/online_store/services/user/internal/domain/models"
+)
+
+type AuthRepository interface {
+	Create(ctx context.Context, email, password, firstName, lastName string) (*models.User, error)
+	FindByEmail(ctx context.Context, email string) (*models.User, error)
+	FindByID(ctx context.Context, ID string) (*models.User, error)
+}
---
+++ services/user/internal/domain/auth/ports/service/service.go
@@ -0,0 +1,14 @@
+package service
+
+import (
+	"context"
+
+	"github.com/BlazeCoder04/online_store/services/user/internal/domain/models"
+)
+
+type AuthService interface {
+	Login(ctx context.Context, email, password string) (*models.User, string, string, error)
+	Register(ctx context.Context, email, password, firstName, lastName string) (*models.User, string, string, error)
+	UpdateTokens(ctx context.Context, refreshToken string) (string, string, error)
+	Logout(ctx context.Context, accessToken string) error
+}
