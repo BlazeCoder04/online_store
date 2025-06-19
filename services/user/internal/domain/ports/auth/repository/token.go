b485732d2feb1080d5d4e8113634b64505267211
BlazeCoder <blaze.coder.work@gmail.com>
2025-06-19 22:25:49 +0300

    refactor(interface): improved naming of parameter of the function - set (token -> refreshToken)

--- services/user/internal/domain/ports/auth/repository/token.go
+++ services/user/internal/domain/ports/auth/repository/token.go
@@ -6,7 +6,7 @@ import (
 )
 
 type TokenRepository interface {
-	Set(ctx context.Context, userID, token string, expiresIn time.Duration) error
+	Set(ctx context.Context, userID, refreshToken string, expiresIn time.Duration) error
 	Get(ctx context.Context, userID string) (string, error)
 	Delete(ctx context.Context, userID string) error
 }
