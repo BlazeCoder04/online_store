93dff5dfacfd011ac81c68844da3b8cf4708ebdb
BlazeCoder <blaze.coder.work@gmail.com>
2025-07-05 19:02:59 +0300

    init: project structure; feat: parse .env (config), create docker-compose (postgres, redis)

---
+++ services/user/configs/config.go
@@ -0,0 +1,41 @@
+package configs
+
+import (
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	ServerEnv  string `mapstructure:"SERVER_ENV"`
+	ServerPort string `mapstructure:"SERVER_PORT"`
+
+	PostgresDSN string `mapstructure:"POSTGRES_DSN"`
+
+	RedisPassword string `mapstructure:"REDIS_PASSWORD"`
+	RedisURI      string `mapstructure:"REDIS_URI"`
+
+	AccessTokenPrivateKey string        `mapstructure:"ACCESS_TOKEN_PRIVATE_KEY"`
+	AccessTokenPublicKey  string        `mapstructure:"ACCESS_TOKEN_PUBLIC_KEY"`
+	AccessTokenExpiresIn  time.Duration `mapstructure:"ACCESS_TOKEN_EXPIRES_IN"`
+
+	RefreshTokenPrivateKey string        `mapstructure:"REFRESH_TOKEN_PRIVATE_KEY"`
+	RefreshTokenPublicKey  string        `mapstructure:"REFRESH_TOKEN_PUBLIC_KEY"`
+	RefreshTokenExpiresIn  time.Duration `mapstructure:"REFRESH_TOKEN_EXPIRES_IN"`
+}
+
+func Load(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+
+	if err = viper.ReadInConfig(); err != nil {
+		return
+	}
+
+	if err = viper.Unmarshal(&config); err != nil {
+		return
+	}
+
+	return
+}
